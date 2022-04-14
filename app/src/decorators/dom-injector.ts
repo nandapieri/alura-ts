af3fddf8fcaf72b6c073175fd5e74267708c34a5
nandapieri <nandapieri@gmail.com>
2022-04-14 16:20:03 -0300

    feat: cacheando o documento html para melhorar desempenho

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,9 +1,13 @@
 export function domInjector (seletor: string ) {
     return function(target: any, propertyKey: string) {
-        //redefinir a propriedade propertyKey para ter um getter e poder pegar a instancia do obj e alterar o valor nessa propriedade
+        //cacheando o elemento html para não precisar pega-lo todas as vezes que acessa alguma propriedade
+        let elemento: HTMLInputElement | null = null;
         const getter = function() {
-            return document.querySelector(seletor);
+            if(!elemento)
+                elemento = <HTMLInputElement>document.querySelector(seletor);
+            return elemento
         }
+        //redefinir a propriedade propertyKey para ter um getter e poder pegar a instancia do obj e alterar o valor nessa propriedade
         Object.defineProperty(target, propertyKey, { get: getter });
     }
 }
