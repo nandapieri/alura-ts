a98b53e84290516bef54d02616cccf9edac70690
nandapieri <nandapieri@gmail.com>
2022-04-14 14:31:50 -0300

    feat: passando parametros para o decorator

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao() { //verificar tempo de execução da funcao decorada
+export function logarTempoDeExecucao(emSegundos: boolean = false) { //verificar tempo de execução da funcao decorada
     return function(
         target: any, //metodo estatico = constructor | metodo não estatico = prototype
         propertyKey: string, //metodo que foi decorado
@@ -6,11 +6,13 @@ export function logarTempoDeExecucao() { //verificar tempo de execução da func
     ) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function(...args: Array<any>) {
+            let divisor = emSegundos? 1000: 1;
+            let unidade = emSegundos? "segundos" : "milisegundos";
             const t1 = performance.now();
             //chamar o metodo original
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/1000} segundos`);
+            console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/divisor} ${unidade}`);
             retorno;
         }
         return descriptor;
