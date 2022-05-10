dd323abf2cc06cf8eccd1bdb0e8ac78cd8adb98d
nandapieri <nandapieri@gmail.com>
2022-05-10 17:41:07 -0300

    feat: make sure that negociacao-controller doesnt import repeated negociacoes

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -68,11 +68,17 @@ export class NegociacaoController {
 
     public importaDados(): void {
         this.negociacoesService.obterNegociacoesDoDia()
-            .then(negociacoesDeHoje => {
-                for(let negociacao of negociacoesDeHoje) {
-                    this.negociacoes.adiciona(negociacao);
-                    this.negociacoesView.update(this.negociacoes);
-                }
-            });
+        .then(negociacoesDeHoje => {
+            return negociacoesDeHoje.filter(negociacaoDeHoje => {
+                return !this.negociacoes.lista()
+                .some(negociacao => negociacao.ehIgual(negociacaoDeHoje))
+            })
+        })
+        .then(negociacoesDeHoje => {
+            for(let negociacao of negociacoesDeHoje) {
+                this.negociacoes.adiciona(negociacao);
+                this.negociacoesView.update(this.negociacoes);
+            }
+        });
     }
 }
